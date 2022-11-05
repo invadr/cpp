0667f896d22f0659d3a8401ef88335b0b377cc77
invadr <117381171+invadr@users.noreply.github.com>
2022-11-05 10:08:45 -0500

    working on vectors in C++

---
+++ vectors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "std_lib_facilities.h"
+
+using namespace std;
+
+int main()
+{
+    vector<int> newVec = {1, 2, 3, 4, 5, 6};
+    vector<string> words = {"Movie Guy", "Top Force Aeroplanes", "Comedic Attempt"};
+     cout << words[1];
+
+    return 0;
+}
