9d1a502d3dee27f5092c754ae22f9c78077abd40
invadr <117381171+invadr@users.noreply.github.com>
2022-11-05 19:13:49 -0500

    updating vectors code

--- vectors.cpp
+++ vectors.cpp
@@ -7,7 +7,16 @@ int main()
 {
     vector<int> newVec = {1, 2, 3, 4, 5, 6};
     vector<string> words = {"Movie Guy", "Top Force Aeroplanes", "Comedic Attempt"};
-     cout << words[1];
+     cout << words[1] << endl;
+     words.push_back("Big Time Potato Chops");
+     cout << words[3] << endl;
+
+    //range based for loop in cpp that provides a succinct way to traverse list-based data structures
+    //like vectors here.
+     for(int b: newVec)
+     {
+        cout << b << endl;
+     }
 
     return 0;
 }
