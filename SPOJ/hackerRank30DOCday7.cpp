9cc680b792242716e1cfec982e5d001ece89280e
invadr <invadr.game.studios@gmail.com>
2022-11-10 13:19:31 -0600

    HackerRank 30 Days of Code - Day 7

--- SPOJ/hackerRank30DOCday7.cpp
+++ SPOJ/hackerRank30DOCday7.cpp
@@ -11,71 +11,72 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
-
-
 int main()
 {
-    string n_temp;
-    getline(cin, n_temp);
+  string n_temp;
+  getline(cin, n_temp);
 
-    int n = stoi(ltrim(rtrim(n_temp)));
+  int n = stoi(ltrim(rtrim(n_temp)));
 
-    string arr_temp_temp;
-    getline(cin, arr_temp_temp);
+  string arr_temp_temp;
+  getline(cin, arr_temp_temp);
 
-    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+  vector<string> arr_temp = split(rtrim(arr_temp_temp));
 
-    vector<int> arr(n);
+  vector<int> arr(n);
 
-    for (int i = 0; i < n; i++) {
-        int arr_item = stoi(arr_temp[i]);
+  for (int i = 0; i < n; i++)
+  {
+    int arr_item = stoi(arr_temp[i]);
 
-        arr[i] = arr_item;
-    }
-    
-    for(int i = arr.size()-1; i >= 0; --i)
-    {
-        cout << arr[i] << " ";
-    }
+    arr[i] = arr_item;
+  }
 
-    return 0;
+  for (int i = arr.size() - 1; i >= 0; --i)
+  {
+    cout << arr[i] << " ";
+  }
+
+  return 0;
 }
 
-string ltrim(const string &str) {
-    string s(str);
+string ltrim(const string &str)
+{
+  string s(str);
 
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
+  s.erase(
+      s.begin(),
+      find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
 
-    return s;
+  return s;
 }
 
-string rtrim(const string &str) {
-    string s(str);
+string rtrim(const string &str)
+{
+  string s(str);
 
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
+  s.erase(
+      find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+      s.end());
 
-    return s;
+  return s;
 }
 
-vector<string> split(const string &str) {
-    vector<string> tokens;
+vector<string> split(const string &str)
+{
+  vector<string> tokens;
 
-    string::size_type start = 0;
-    string::size_type end = 0;
+  string::size_type start = 0;
+  string::size_type end = 0;
 
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
+  while ((end = str.find(" ", start)) != string::npos)
+  {
+    tokens.push_back(str.substr(start, end - start));
 
-        start = end + 1;
-    }
+    start = end + 1;
+  }
 
-    tokens.push_back(str.substr(start));
+  tokens.push_back(str.substr(start));
 
-    return tokens;
+  return tokens;
 }
