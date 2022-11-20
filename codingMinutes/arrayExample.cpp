fe0f04933f466fe234a0c557d7219a8ddd056901
invadr <invadr.game.studios@gmail.com>
2022-11-19 20:27:29 -0600

    more DSA

---
+++ codingMinutes/arrayExample.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int marks[100];
+
+    int n;
+    cout<<"Enter the number of students: ";
+    cin>>n;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>marks[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        cout<<marks[i]<< ", ";
+    }
+    cout << endl;
+
+    return 0;
+}
