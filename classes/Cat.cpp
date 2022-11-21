cbfb1b6ed911760186f095adfc8173b54fe55457
invadr <117381171+invadr@users.noreply.github.com>
2022-11-20 23:23:19 -0600

    working on OOP

--- classes/Cat.cpp
+++ classes/Cat.cpp
@@ -12,3 +12,23 @@ void Cat::jump()
 {
 	cout << "Jumping to top of Bookcase" << endl;
 }
+
+void Cat::setName(string name) 
+{
+	this->name = name;
+}
+
+void Cat::setAge(int age) 
+{
+	this->age = age;
+}
+
+int Cat::getAge()
+{
+	return age;
+}
+
+string Cat::getName()
+{
+	return name;
+}
--- classes/Cat.h
+++ classes/Cat.h
@@ -2,8 +2,16 @@
 
 class Cat
 {
+private:
+	string name;
+	int age;
 public:
+	Cat(string name, int age): name(name), age(age) {};
 	void speak();
 	void jump();
+	int getAge();
+	string getName();
+	void setAge(int age);
+	void setName(string name);
 };
 
